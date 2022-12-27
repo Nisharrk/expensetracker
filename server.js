3263bcab16efbc616b4a5cc8e2f3e83efe59218d
Your Name <you@example.com>
2022-12-27 16:52:57 -0500

    Server working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.json("message", "This is a server");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
